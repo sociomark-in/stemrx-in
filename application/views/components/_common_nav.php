07d908cf5082ccb941ba0642167124a57be045bb
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-20 15:48:57 +0530

    CI3 StemRx

---
+++ application/views/components/_common_nav.php
@@ -0,0 +1,128 @@
+<nav class="navbar navbar-expand-md bg-body-tertiary p-md-0">
+	<div class="container ">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSubNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-between" id="navbarSubNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="#"><i class="fa-solid fa-magnifying-glass me-2"></i>Find a Doctor</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><i class="fa-solid fa-shop me-2"></i>Shop</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="main-nav">
+	<nav class="navbar navbar-expand-md">
+		<div class="container">
+			<a class="navbar-brand" href="#">
+				<img src="<?= base_url("assets/media/") ?>images/stemrx-website-logo.png" alt="" height="45">
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+				<ul class="navbar-nav align-items-center">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item dropdown dropdown-hover position-static">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Specialities
+						</a>
+						<!-- Dropdown menu -->
+						<div class="dropdown-menu w-100 mt-0" aria-labelledby="navbarDropdown" style="border-top-left-radius: 0;
+								border-top-right-radius: 0;
+							  ">
+
+							<div class="container">
+								<div class="row my-4">
+									<div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+										<div class="list-group list-group-flush">
+											<a href="" class="list-group-item list-group-item-action">Lorem ipsum</a>
+											<a href="" class="list-group-item list-group-item-action">Dolor sit</a>
+											<a href="" class="list-group-item list-group-item-action">Amet consectetur</a>
+											<a href="" class="list-group-item list-group-item-action">Cras justo odio</a>
+											<a href="" class="list-group-item list-group-item-action">Adipisicing elit</a>
+										</div>
+									</div>
+									<div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+										<div class="list-group list-group-flush">
+											<a href="" class="list-group-item list-group-item-action">Explicabo voluptas</a>
+											<a href="" class="list-group-item list-group-item-action">Perspiciatis quo</a>
+											<a href="" class="list-group-item list-group-item-action">Cras justo odio</a>
+											<a href="" class="list-group-item list-group-item-action">Laudantium maiores</a>
+											<a href="" class="list-group-item list-group-item-action">Provident dolor</a>
+										</div>
+									</div>
+									<div class="col-md-6 col-lg-3 mb-3 mb-md-0">
+										<div class="list-group list-group-flush">
+											<a href="" class="list-group-item list-group-item-action">Iste quaerato</a>
+											<a href="" class="list-group-item list-group-item-action">Cras justo odio</a>
+											<a href="" class="list-group-item list-group-item-action">Est iure</a>
+											<a href="" class="list-group-item list-group-item-action">Praesentium</a>
+											<a href="" class="list-group-item list-group-item-action">Laboriosam</a>
+										</div>
+									</div>
+									<div class="col-md-6 col-lg-3">
+										<div class="list-group list-group-flush">
+											<a href="" class="list-group-item list-group-item-action">Cras justo odio</a>
+											<a href="" class="list-group-item list-group-item-action">Saepe</a>
+											<a href="" class="list-group-item list-group-item-action">Vel alias</a>
+											<a href="" class="list-group-item list-group-item-action">Sunt doloribus</a>
+											<a href="" class="list-group-item list-group-item-action">Cum dolores</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Media Center</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+					</li>
+					<li class="nav-item">
+						<a class="btn btn-primary" href="#">Call Us Now</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</div>
+<nav class="navbar navbar-expand-md bg-body-tertiary navbar-bottom-fixed">
+	<div class="container justify-content-center" id="navbarSubNav">
+		<ul class="navbar-nav gap-3">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Request a Callback</a>
+			</li>
+			<li class="nav-item">
+				<a class="btn btn-primary" href="#">Book Appointment</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Get health Checkup</a>
+			</li>
+		</ul>
+	</div>
+</nav>
