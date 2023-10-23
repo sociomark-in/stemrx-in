23bba3fb8b3f9eaaa3af77c40fc99dc590aabb22
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-23 17:54:59 +0530

    Config Changes

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost:8000/stemrx-demo/';
+$config['base_url'] = 'https://test.apogeedigitech.in/repositories/stemrx-in/';
 
 /*
 |--------------------------------------------------------------------------
