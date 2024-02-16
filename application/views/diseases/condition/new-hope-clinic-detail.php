0246b85de6e8c414c51b1348e2bbe25239e73d41
kunalmahajan976757 <kunalmcamahajan@gmail.com>
2024-02-16 19:07:03 +0530

    New Hope Clinic Details

---
+++ application/views/diseases/condition/new-hope-clinic-detail.php
@@ -0,0 +1,359 @@
+<main>
+	<!-- Breadcumb -->
+	<div class="breadcumb-wrapper ">
+		<div class="parallax" data-parallax-image="<?= base_url("assets/") ?>img/breadcurmb/breadcurmb-1-1.jpg"></div>
+		<div class="container z-index-common">
+			<div class="breadcumb-content">
+				<h1 class="breadcumb-title">Ovary Rejuvenation Details</h1>
+				<div class="breadcumb-menu-wrap">
+					<i class="far fa-home-lg"></i>
+					<ul class="breadcumb-menu">
+						<li><a href="index.html">Home</a></li>
+						<li class="active">Ovary Rejuvenation Details</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--
+    Service Area
+    -->
+	<section class="vs-service-wrapper space-top space-md-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="service-content mb-30">
+						<div class="vs-surface wow" data-wow-delay="0.3s">
+							<img src="<?= base_url("assets/") ?>img/service/sr-d-1.jpg" alt="Service Image" class="w-100">
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-xl-11 col-xxl-10 ">
+								<div class="service-bar">
+									<div class="row justify-content-between align-items-center gy-4 text-center text-lg-start">
+										<div class="col-sm-6 col-lg-auto">
+											<span class="fs-xs">Condition Name</span>
+											<h2 class="h5 mb-0">Ovary Rejuvenation</h2>
+										</div>
+										<div class="col-sm-6 col-lg-auto">
+											<span class="fs-xs">Time Duration</span>
+											<h2 class="h5 mb-0">More Than 12 Hour</h2>
+										</div>
+										<div class="col-sm-6 col-lg-auto">
+											<span class="fs-xs">Doctor Name</span>
+											<h2 class="h5 mb-0">Dr. Pallavi & Dr. Mahajan</h2>
+										</div>
+										<div class="col-sm-6 col-lg-auto">
+											<a href="#appointment" class="vs-btn style2">Appointment<i class="fal fa-calendar-alt"></i></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<section class="appointment-wrapper">
+							<div class="container">
+								<h3>Symptoms:</h3>
+								<div class="row gx-40">
+									<div class="col-xl-12">
+										<ul>
+											<li>Irregular menstrual cycles</li>
+											<li>Difficulty conceiving</li>
+											<li>Premature menopause</li>
+											<li>Hormonal imbalances</li>
+											<li>Decreased ovarian reserve</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</section>
+
+						<section class="appointment-wrapper">
+							<div class="container">
+								<h3>Fear and concerns:</h3>
+								<p>Many individuals facing ovarian issues fear the loss of fertility and the emotional toll associated with difficulty conceiving. They may also worry about the potential side effects or invasiveness of traditional treatments. Additionally, concerns about the financial burden of fertility treatments can add stress.</p>
+								<div class="row gx-40">
+									<div class="col-xl-12">
+										<ul>
+											<li><b class="text-title">Concerns:</b> Fear of infertility and inability to conceive or carry a child.</li>
+											<li><b class="text-title">Hormonal imbalances:</b> Anxiety about the physical and emotional effects of hormonal fluctuations.</li>
+											<li><b class="text-title">Menopausal symptoms:</b> Concerns about experiencing symptoms such as hot flashes, mood swings, and vaginal dryness at a young age.</li>
+											<li><b class="text-title">Health risks:</b> Worries about long-term health consequences, including osteoporosis and cardiovascular disease.</li>
+											<li><b class="text-title">Emotional distress:</b> Fear of loss of femininity, identity, and the impact on relationships.</li>
+											<li><b class="text-title">Financial burden: </b>Stress over the cost of fertility treatments or alternative family-building options.</li>
+											<li><b class="text-title">Social stigma:</b> Fear of judgment or misunderstanding from peers or society about their condition.</li>
+											<li><b class="text-title">Future planning:</b> Anxiety about career disruptions, relationship dynamics, and overall life trajectory</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</section>
+
+						<section class="appointment-wrapper">
+							<div class="container">
+								<h3>Overview of StemRx's innovative treatment approach:</h3>
+								<p>Ovarian rejuvenation is performed through cutting-edge mesenchymal stem cell therapy combined with the regenerative power of extracellular matrix derived from the same organ. At StemRx, we employ the latest advancements, including Lyophilized Platelet Rich Plasma (LPRP), harnessing the potential of quantum energy medicine to revolutionize treatment outcomes.</p>
+								<p>Our approach extends beyond conventional methods; we integrate peptides and growth hormones tailored to enhance circulation and lymphatic drainage, optimizing the targeted area. Through meticulous detoxification protocols, we pave the way for cellular revitalization.</p>
+								<p>Furthermore, our comprehensive treatment includes strategic stimulations designed to synchronize impulses from the hypothalamus and pituitary glands to the ovaries, facilitating precise signaling crucial for natural conception. </p>
+							</div>
+						</section>
+
+						<section class="appointment-wrapper">
+							<div class="container">
+								<h3>Here are the key features and benefits of StemRx’s Treatment plan:</h3>
+								<div class="row gx-40">
+									<div class="col-xl-12">
+										<ul>
+											<li><b class="text-title">Non-invasive Procedure:</b> Unlike traditional surgical interventions, StemRx's treatment is minimally invasive, reducing the risk of complications and recovery time for patients.</li>
+											<li><b class="text-title">Stem Cell Therapy:</b> Utilizing cutting-edge stem cell technology, StemRx rejuvenates ovarian tissue, promoting the production of healthier eggs and restoring fertility potential.</li>
+											<li><b class="text-title">Personalized Treatment Plans:</b> StemRx tailors each treatment plan to the individual needs of the patient, optimizing outcomes and increasing the chances of successful conception.</li>
+											<li><b class="text-title">Natural and Safe:</b> StemRx's approach harnesses the body's natural regenerative capabilities, offering a safe and sustainable solution for ovarian health restoration.</li>
+											<li><b class="text-title">Long-lasting Results:</b> Patients undergoing StemRx's treatment can experience long-lasting improvements in ovarian function, providing renewed hope for building a family.</li>
+											<li><b class="text-title">Holistic Support:</b> StemRx prioritizes holistic patient care, offering comprehensive support throughout the treatment journey, including counseling, nutritional guidance, and emotional and psychological support.</li>
+
+										</ul>
+									</div>
+								</div>
+							</div>
+						</section>
+						<!-- <p class="fs-md text-title mb-4 pb-2">Professionally iterate out-of-the-box relationships without scalable "outside the box" thinking. Intrinsicly revolutionize team building customer service before cross-platform portals. Quickly plagiarize resource maximizing mindshare and state of the art deliverables. Phosfluorescently reintermediate sustainable architectures for enterprise relationships. Compellingly actualize world-class solutions for high-payoff initiatives.</p>
+						<p>Monotonectally foster alternative technology vis-a-vis multifunctional leadership. Compellingly orchestrate standards compliant schemas for highly efficient interfaces. Uniquely impact orthogonal customer service whereas standards compliant services. Professionally communicate performance based niche markets without performance based information. Objectively plagiarize prospective networks via ubiquitous web-readiness. Credibly monetize process-centric synergy with intuitive strategic theme areas. Professionally provide access to optimal portals without dynamic supply chains. Enthusiastically re-engineer equity invested imperatives without leveraged alignments. Monotonectally scale parallel methods of empowerment rather than wireless sources. Competently scale robust relationships without maintainable synergy. Completely enhance best-of-breed models for ubiquitous applications. Quickly underwhelm bricks-and-clicks bandwidth with resource maximizing e-services. Appropriately incentivize out-of-the-box relationships after customized users. Continually productivate real-time testing procedures and backward-compatible scenarios. Holisticly predominate enabled ideas whereas future-proof content.</p>
+						<div class="row mt-40 mb-20">
+							<div class="col-lg-4 mb-30">
+								<img src="<?= base_url("assets/") ?>img/service/sr-d-2.jpg" alt="Service Image" class="w-100">
+							</div>
+							<div class="col-lg-4 mb-30">
+								<img src="<?= base_url("assets/") ?>img/service/sr-d-3.jpg" alt="Service Image" class="w-100">
+							</div>
+							<div class="col-lg-4 mb-30">
+								<img src="<?= base_url("assets/") ?>img/service/sr-d-4.jpg" alt="Service Image" class="w-100">
+							</div>
+						</div>
+						<h3>Granular potentialities oriented</h3>
+						<p>Authoritatively disseminate multimedia based total linkage through market-driven methodologies. Continually transform integrated results vis-a-vis multidisciplinary manufactured products. Appropriately foster fully researched innovation rather than backend supply chains results vis-a-vis multidisciplin ary manufactured. Synergistically underwhelm distinctive strategic theme areas for low-risk high-yield vortals. Seamlessly fabricate high-quality portals and next-generation human capital. Progressively network extensive leadership for client-focused e-markets. Interactively whiteb ilers for cost effective synergy.</p>-->
+					</div>
+				</div>
+			</div>
+
+			<div class="row justify-content-center">
+				<div class="col-xl-10 col-12">
+					<div class="w-100">
+						<iframe class="ratio ratio-16x9" src="https://www.youtube.com/embed/9UyC88VHt9U?si=mGZB_RPXoSDpF_ij&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="vs-specialities-wrapper space-md-bottom">
+		<div class="container">
+			<div class="row text-center justify-content-center mb-30">
+				<div class="col-md-10 col-lg-8 wow fadeInUp" data-wow-delay="0.3s">
+					<h2 class="h1">Our Treatments at Stemrx</h2>
+					<p>Proactively revolutionize granular customer service after pandemic internal or "organic" sources proactive human capital rather.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-lg-9 col-md-11 col-12">
+				<div class="row g-4 justify-content-center">
+					<?php for ($i = 0; $i < 10; $i++) : ?>
+						<div class="col-2">
+							<a href="<?= base_url("therapeutics/therapy-single") ?>" class="text-center">
+								<div class="card bg-white sp-item mb-2">
+									<div class="card-body">
+										<img src="<?= base_url() ?>assets/img/logo.png" alt="Speciality" class="w-100">
+									</div>
+								</div>
+								<p>Treatment Single</p>
+							</a>
+						</div>
+					<?php endfor ?>
+				</div>
+			</div>
+		</div>
+		</div>
+	</section>
+
+	<section class="vs-team-wrapper space-md-bottom">
+		<div class="container">
+			<div class="team-content text-center mb-30">
+				<!-- <span class="sec-subtitle text-theme h3 mb-2 mb-sm-0">StemRx Medical Care!</span> -->
+				<h2 class="h1 mb-3">Our Consultant for Diseses</h2>
+			</div>
+			<div class="row">
+				<?php for ($i = 0; $i < 4; $i++) : ?>
+					<div class="col-md-6 col-lg-4 col-xl-3 mb-30 wow fadeInUp" data-wow-delay="0.3s">
+						<div class="team-card">
+							<div class="team-head"><img src="<?= base_url() ?>assets/img/team/t-1-1.png" alt="Team Area" class="w-100">
+								<!-- <div class="team-card-links"><a class="team-links-toggler" href="#"><i class="fas fa-share-alt"></i></a>
+									<div class="team-social"><a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fas fa-basketball-ball"></i></a></div>
+								</div> -->
+							</div>
+							<div class="team-body">
+								<h3 class="h4 mb-0"><a href="<?= base_url() ?>doctor/doctor-<?= $i ?>-name" class="text-reset">David Smith</a></h3>
+								<p class="fs-xs degi text-theme mb-2">Specialist</p>
+								<p class="fs-xs">Conceptualize user-centric web-readiness via economically sound e-services. Interactively coordinate next-generation</p>
+								<div class="">
+									<p class="fs-xs team-info"><i class="fas fa-phone-alt text-theme"></i><a class="text-reset" href="tel:+592201520156">+592 2015 20156</a></p>
+									<p class="fs-xs team-info"><i class="fas fa-envelope text-theme"></i><a class="text-reset" href="mailto:info.example@mail.com">info.example@mail.com</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endfor ?>
+			</div>
+		</div>
+	</section>
+
+	<section class="vs-skill-wrapper space-md-bottom d-none">
+		<div class="container">
+			<div class="skill-wrap1 bg-white">
+				<div class="row justify-content-center justify-content-lg-between">
+					<div class="col-md-6 col-lg-auto mb-30">
+						<div class="d-xl-flex text-center text-xl-start skill-box"><span class="ripple-icon hover-style2 align-self-start mb-20 mb-xl-0 mr-20"><i class="flaticon-discuss"></i></span>
+							<div class="media-body">
+								<h2 class="mt-n2 mb-0 text-theme">30+</h2>
+								<p class="text-title fs-md fw-medium mt-1 mt-xl-0 mb-2 mb-xl-2">Years Of Experience</p>
+								<p class="fs-xs mb-0">Incubate extensive scenarios without top-line quality vectors. Authoritatively engage</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-auto mb-30">
+						<div class="d-xl-flex text-center text-xl-start skill-box"><span class="ripple-icon hover-style2 align-self-start mb-20 mb-xl-0 mr-20"><i class="flaticon-medical-equipment"></i></span>
+							<div class="media-body">
+								<h2 class="mt-n2 mb-0 text-theme">100+</h2>
+								<p class="text-title fs-md fw-medium mt-1 mt-xl-0 mb-2 mb-xl-2">Experienced Doctor's</p>
+								<p class="fs-xs mb-0">Incubate extensive scenarios without top-line quality vectors. Authoritatively engage</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-auto mb-30">
+						<div class="d-xl-flex text-center text-xl-start skill-box"><span class="ripple-icon hover-style2 align-self-start mb-20 mb-xl-0 mr-20"><i class="flaticon-healthcare"></i></span>
+							<div class="media-body">
+								<h2 class="mt-n2 mb-0 text-theme">200+</h2>
+								<p class="text-title fs-md fw-medium mt-1 mt-xl-0 mb-2 mb-xl-2">Happy Patients</p>
+								<p class="fs-xs mb-0">Incubate extensive scenarios without top-line quality vectors. Authoritatively engage</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="testimonial-wrapper space-bottom">
+		<div class="container">
+			<div class="row text-center justify-content-center">
+				<div class="col-md-10 col-lg-8 col-xl-6 wow fadeInUp" data-wow-delay="0.3s">
+					<div class="section-title">
+						<!-- <span class="h3 text-theme sec-subtitle">Testimonial</span> -->
+						<h2 class="h1">Our Patients Responses</h2>
+						<p>Proactively revolutionize granular customer service after pandemic internal or "organic" sources proactive human capital rather.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-xl-10">
+					<div class="row vs-carousel wow fadeInUp" data-wow-delay="0.3s" data-slide-show="2">
+						<div class="col-xl-6">
+							<a href="https://www.youtube.com/watch?v=_m5XOHuJAxI&feature=youtu.be" class="ratio-16x9" target="_blank">
+								<div class="youtube-testimonial">
+									<img src="<?= base_url() ?>assets/img/testimonial/thumbnail-1.jpg" alt="">
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-6">
+							<a href="https://www.youtube.com/watch?v=_m5XOHuJAxI&feature=youtu.be" class="ratio-16x9" target="_blank">
+								<div class="youtube-testimonial">
+									<img src="<?= base_url() ?>assets/img/testimonial/thumbnail-1.jpg" alt="">
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-6">
+							<a href="https://www.youtube.com/watch?v=_m5XOHuJAxI&feature=youtu.be" class="ratio-16x9" target="_blank">
+								<div class="youtube-testimonial">
+									<img src="<?= base_url() ?>assets/img/testimonial/thumbnail-1.jpg" alt="">
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="appointment-wrapper space-md-bottom" id="appointment">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-6 mb-30">
+					<div class="about-content">
+						<!-- <span class="sec-subtitle text-theme h3 mb-2 mb-sm-0">StemRx Medical Care!</span> -->
+						<h2 class="h1 mb-3">We're Here For You</h2>
+						<div class="row">
+							<div class="col-xl-10 mb-md-2 pb-md-1">
+								<p class="mb-md-4">Proactively revolutionize granular customer service after pandemic internal or "organic" sources. Distinctively impact proactive human capital rather than client-centered benefits.</p>
+							</div>
+							<div class="col-md-6 col-lg-5 col-xl-4">
+								<ul class="vs-list list-unstyled text-title">
+									<li>99% success rate</li>
+									<li>100% Confidential</li>
+									<li>Affordable Fees</li>
+								</ul>
+							</div>
+							<div class="col-md-6 col-lg-5 col-xl-5">
+								<ul class="vs-list list-unstyled text-title">
+									<li>Free Consultation</li>
+									<li>Expert surveillance agents</li>
+									<li>Over 50 years experience</li>
+								</ul>
+							</div>
+						</div>
+						<div class="d-none">
+							<ul class="offer-counter counter-list list-unstyled" data-offer-date="01/01/2024">
+								<li>
+									<div class="day count-number">00</div><span class="count-name">Days</span>
+								</li>
+								<li>
+									<div class="hour count-number">00</div><span class="count-name">Hours</span>
+								</li>
+								<li>
+									<div class="minute count-number">00</div><span class="count-name">Minutes</span>
+								</li>
+								<li>
+									<div class="seconds count-number">00</div><span class="count-name">Seconds</span>
+								</li>
+							</ul>
+							<div class="vs-code-box mt-20">Use Code: <span class="text-theme">Native</span></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 mb-30 pt-30 pt-xl-0">
+					<div class=" bg-white wow fadeInUp">
+						<?php
+						$data = [
+							"action" => [
+								"method" => "POST",
+								"action" => base_url()
+							],
+							"title" => [
+								'bg-image' => "",
+								'heading' => "Book An Appointment",
+								'desc' => "Please Call Us To Ensure",
+							],
+							"props" => [
+								'class' => "shadow-none mb-0",
+								'bg-image' => "",
+								'bg-color' => "#f3f6f7",
+							],
+							"fields" => [
+								"condition" => "disabled",
+								"doctor" => "enabled",
+							]
+						];
+						$this->load->view('common/forms/appointment_form', $data); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
