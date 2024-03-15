a42070f7bcd1dfb08040b7c44647691f21b118ae
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-03-15 20:06:28 +0530

    Dr. Pallavi Lele

---
+++ application/views/pages/team/dr-pallavi-lele.php
@@ -0,0 +1,215 @@
+<main>
+	<section class="vs-details-wrapper space-top">
+		<div class="container">
+			<div class="row gx-40">
+				<div class="col-lg-5">
+					<div class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+						<div class="member-details-img"><img src="https://placehold.co/600x800" alt="Member Image" class="w-100"></div>
+						<div class="member-angle-links">
+							<div class="shape"><img src="<?= base_url() ?>assets/img/bg/member-shape.png" alt="Shape"></div>
+							<!-- <span class="middle-icon"><i class="fas fa-share-alt"></i></span> 
+							<a href="#"><i class="fab fa-facebook-f"></i></a> 
+							<a href="#"><i class="fa fa-link"></i></a> 
+							<a href="#"><i class="fab fa-twitter"></i></a> 
+							<a href="#"><i class="fa fa-link"></i></a> -->
+						</div>
+						<a href="#" class="certifate-link">See All Publications<i class="far fa-chevron-circle-right"></i></a>
+					</div>
+				</div>
+				<div class="col-lg-7">
+					<div class="team-content">
+						<h2 class="mb-0 mt-n2">Dr. Pallavi Lele</h2>
+						<p class="text-theme fs-xs">MBBS, MD, Obstetrician & Gynecologist, FMAS Infertility Specialist, Cosmetic Gynecologist, Laparoscopic Surgeon</p>
+						<p class="fs-md text-title">Dr. Pallavi Lale is a highly skilled Obstetrician & Gynecologist. She is dedicated to providing comprehensive care to her patients, particularly in the areas of infertility treatment and gynaecological surgeries.</p>
+						<p>
+							Dr Lale is renowned for her expertise in ovarian rejuvenation, cosmetic gynaecological procedures, and infertility management, making her internationally recognised as one of the top infertility gynaecologists.
+						</p>
+						<table class="member-table">
+							<tbody>
+								<tr>
+									<th><strong>Speciality</strong></th>
+									<td>FMAS Infertility</td>
+								</tr>
+								<tr>
+									<th><strong>Degrees</strong></th>
+									<td><span class="text-theme">MBBS, M.D</span></td>
+								</tr>
+								<tr>
+									<th><strong>Areas of Expertise</strong></th>
+									<td>
+										<ul class="member-info-list">
+											<li>ovarian rejuvenation</li>
+											<li>cosmetic gynaecological procedures</li>
+											<li>infertility management</li>
+										</ul>
+									</td>
+								</tr>
+								<!-- <tr>
+									<th><strong>Office</strong></th>
+									<td>374 William S Canning Blvd, Fall River MA 2721</td>
+								</tr>
+								<tr>
+									<th><strong>University</strong></th>
+									<td>Harvard University</td>
+								</tr> -->
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="vs-specialities-wrapper space-md-bottom">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-10 col-12">
+					<div class="w-100 space">
+						<iframe class="ratio ratio-16x9" src="https://www.youtube.com/embed/_m5XOHuJAxI?si=kGpwhMWmF9nP_zZU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+			<div class="row mb-30">
+				<div class="col-12">
+					<h2 class="mb-0 mt-n2">Area of Expertise (Conditions & Therapeutics)</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center space-md-bottom">
+				<div class="col-xl-11 col-12">
+					<div class="row g-4 justify-content-center">
+						<div class="col-lg-2 col-4">
+							<a href="<?= base_url() ?>" class="text-center">
+								<div class="card bg-white sp-item mb-2">
+									<div class="card-body">
+										<img src="<?= base_url() ?>assets/img/logo.png" alt="Speciality" class="w-100">
+									</div>
+								</div>
+								<p>Cosmetic Gynaecology</p>
+							</a>
+						</div>
+						<div class="col-lg-2 col-4">
+							<a href="<?= base_url() ?>" class="text-center">
+								<div class="card bg-white sp-item mb-2">
+									<div class="card-body">
+										<img src="<?= base_url() ?>assets/img/logo.png" alt="Speciality" class="w-100">
+									</div>
+								</div>
+								<p>Infertility Management</p>
+							</a>
+						</div>
+						<div class="col-lg-2 col-4">
+							<a href="<?= base_url() ?>" class="text-center">
+								<div class="card bg-white sp-item mb-2">
+									<div class="card-body">
+										<img src="<?= base_url() ?>assets/img/logo.png" alt="Speciality" class="w-100">
+									</div>
+								</div>
+								<p>Ovarian Rejuvenation</p>
+							</a>
+						</div>
+						<div class="col-12">
+							<div class="text-center">
+								<a href="" class="vs-btn">View All Conditions</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center d-none">
+				<div class="col-xl-11 col-12">
+					<div class="row g-4">
+						<?php for ($i = 0; $i < 6; $i++) : ?>
+							<div class="col-lg-2 col-4">
+								<a href="<?= base_url() ?>" class="text-center">
+									<div class="card bg-white sp-item mb-2">
+										<div class="card-body">
+											<img src="<?= base_url() ?>assets/img/logo.png" alt="Speciality" class="w-100">
+										</div>
+									</div>
+									<p>Lorem ipsum dolor sit.</p>
+								</a>
+							</div>
+						<?php endfor ?>
+						<div class="col-12">
+							<div class="text-center">
+								<a href="" class="vs-btn">View All Therapeutics</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="vs-project-wrapper d-none">
+		<div class="container z-index-common">
+			<div class="row text-center justify-content-center">
+				<div class="col-md-10 col-lg-8 col-xl-6 wow fadeInUp" data-wow-delay="0.3s">
+					<div class="section-title"><span class="h3 text-theme sec-subtitle">Achievements</span>
+						<h2 class="h1">Awards & Accolates</h2>
+						<p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, praesentium.</p>
+						<!--</div>-->
+					</div>
+				</div>
+				<div class="row vs-carousel wow fadeInUp" data-wow-delay="0.3s" data-slide-show="5">
+					<?php for ($i = 0; $i < 10; $i++) : ?>
+						<div class="col-md-6 col-lg-4 col-xl-3 filter-item cat-2">
+							<div class="vs-project-box mb-30">
+								<div class="project-img"><a href="https://placehold.co/334x480" class=" popup-image"><img src="https://placehold.co/334x480" alt="Project Image" class="w-100"></a></div>
+							</div>
+						</div>
+					<?php endfor ?>
+				</div>
+			</div>
+	</section>
+	<section>
+		<div class="container">
+			<div class="row gx-40">
+				<div class="col-lg-7">
+					<?php
+					$data = [
+						"action" => [
+							"method" => "POST",
+							"action" => base_url()
+						],
+						"title" => [
+							'bg-image' => "",
+							'heading' => "Book An Appointment",
+							'desc' => "Please Call Us To Ensure",
+						],
+						"props" => [
+							'class' => "form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp",
+							'bg-image' => "",
+							'bg-color' => "#f3f6f7",
+						],
+						"fields" => [
+							'condition' => "enabled",
+							'doctor' => "disabled"
+						]
+					];
+					$this->load->view('common/forms/appointment_form', $data); ?>
+				</div>
+				<div class="col-lg-5">
+					<div class="team-schedule bg-smoke wow fadeInUp" data-wow-delay="0.3s">
+						<h3 class="h4 border-title">Timings & Schedule</h3>
+						<table class="team-schedule-table">
+							<tbody>
+								<tr>
+									<td>Mon - Fri:</td>
+									<td>8.00 am - 8.00 pm</td>
+								</tr>
+								<tr>
+									<td>Saturday:</td>
+									<td>9.00 am - 6.00 pm</td>
+								</tr>
+								<tr>
+									<td>Sunday:</td>
+									<td>9.00 am - 6.00 pm</td>
+								</tr>
+							</tbody>
+						</table>
+						<a href="tel:919979976862" class="vs-btn style2">+91 997 997 6862<i class="fas fa-phone-alt"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
