2c254800121c69d6c72d3f46514997f278d464a8
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-03-15 19:36:31 +0530

    Team

--- application/views/pages/team/home.php
+++ application/views/pages/team/home.php
@@ -15,6 +15,35 @@
 	</div>
 	<section class="vs-team-wrapper space">
 		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-6 col-xl-4 mb-30 wow fadeInUp" data-wow-delay="0.3s">
+					<div class="team-card">
+						<div class="team-head"><img src="https://placehold.co/380x320" alt="Team Area" class="w-100">
+							<div class="team-card-links"><a class="team-links-toggler" href="javascript:void(0)"><i class="fas fa-eye"></i></a>
+								<!-- <div class="team-social"><a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fas fa-basketball-ball"></i></a></div> -->
+							</div>
+						</div>
+						<div class="team-body p-3">
+							<h3 class="h4 mb-0"><a href="javascript:void(0)" class="text-reset">Dr. Pradeep Mahajan</a></h3>
+							<p class="fs-xs degi text-theme mb-2">MD (Mum), MD (USA), </p>
+							<p class="fs-xs">Senior Consultant Assistant to Dr. Mahajan in day-to-day clinical management and administration of the hospital.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-xl-8">
+					<div class="body">
+						<div class="mb-md-4 mb-3">
+							<h3>You Carry Your Own Repairing Kits... in Your Body!</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque vero esse repudiandae facere magnam inventore aliquid voluptatum porro, sunt ad nostrum voluptatem quae possimus doloremque a iure ullam fugit nam!
+							</p>
+						</div>
+						<a href="" class="vs-btn style2 mt-30 mt-md-0">
+							<i class="far fa-long-arrow-right"></i>Know More about Dr. Mahajan
+						</a>
+					</div>
+				</div>
+			</div>
 			<div class="row">
 				<div class="col-md-6 col-xl-4 mb-30 wow fadeInUp" data-wow-delay="0.3s">
 					<div class="team-card">
@@ -72,7 +101,7 @@
 						<div class="team-body">
 							<h3 class="h4 mb-0"><a href="javascript:void(0)" class="text-reset">Dr. Pallavi Lale</a></h3>
 							<p class="fs-xs degi text-theme mb-2">Laparoscopic Surgeon </p>
-							<p class="fs-xs">Infertility & Regenerative Medicine Consultant, Laparoscopic Surgeon  </p>
+							<p class="fs-xs">Infertility & Regenerative Medicine Consultant, Laparoscopic Surgeon </p>
 							<div class="d-none">
 								<p class="fs-xs team-info"><i class="fas fa-phone-alt text-theme"></i><a class="text-reset" href="tel:+592201520156">+592 2015 20156</a></p>
 								<p class="fs-xs team-info"><i class="fas fa-envelope text-theme"></i><a class="text-reset" href="mailto:info.example@mail.com">info.example@mail.com</a></p>
